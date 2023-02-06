2bee10e038ec51b1b8c188f1dbd76489dd5d076f
Alex Bergsland <alexbergsland@gmail.com>
2023-02-06 12:26:43 +0100

    Update deviceinfo.py

--- deviceinfo.py
+++ deviceinfo.py
@@ -33,18 +33,23 @@ mhostname = mhostname.lower() # make lowecase
 
 # get memory info in megabytes
 tot_m, used_m, free_m, shared_m, buff_m, available_m = map(int, os.popen('free -t -m').readlines()[-3].split()[1:])
+# Use the line below if you are interested in the total of ram + swap
+#tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
 
 # get diskspace info in bytes
 total_disk, used_disk, free_disk = shutil.disk_usage("/")
 
+# bytes to terrabytes
 def toTB(mybytes):
     mytb = round(mybytes / 1099511627776, 1)
     return mytb
 
+# megabytes to gigabytes
 def toGB(mybytes):
     mygb = round(mybytes / 1073741824, 1)
     return mygb
 
+# megabytes to gigabytes
 def mb_to_GB(mymegabytes):
     mygb = round(mymegabytes * 0.0009765625, 1)
     return mygb
@@ -54,19 +59,18 @@ total_disk = toGB(total_disk)
 used_disk = toGB(used_disk)
 free_disk = toGB(free_disk)
 
-
 # minne
-print("input_number." + mhostname + "_minne_totalt")
-setdata("input_number." + mhostname + "_minne_totalt", tot_m, mhostname + " minne totalt", "MB", "mdi:memory")
-print("input_number." + mhostname + "_minne_anvant")
-setdata("input_number." + mhostname + "_minne_anvant", used_m, mhostname + " minne använt", "MB", "mdi:memory")
-print("input_number." + mhostname + "_minne_ledigt")
-setdata("input_number." + mhostname + "_minne_ledigt", free_m, mhostname + " minne ledigt", "MB", "mdi:memory")
+print("input_number." + mhostname + "_memory_total")
+setdata("input_number." + mhostname + "_memory_total", tot_m, mhostname + " memory total", "MB", "mdi:memory")
+print("input_number." + mhostname + "_memory_used")
+setdata("input_number." + mhostname + "_memory_used", used_m, mhostname + " memory used", "MB", "mdi:memory")
+print("input_number." + mhostname + "_memory_free")
+setdata("input_number." + mhostname + "_memory_free", free_m, mhostname + " memory free", "MB", "mdi:memory")
 
 # hdd
-print("input_number." + mhostname + "_hdd_totalt")
-setdata("input_number." + mhostname + "_hdd_totalt", total_disk, mhostname + " hdd totalt", "GB", "mdi:harddisk")
-print("input_number." + mhostname + "_hdd_anvant")
-setdata("input_number." + mhostname + "_hdd_anvant", used_disk, mhostname + " hdd använt", "GB", "mdi:harddisk")
-print("input_number." + mhostname + "_hdd_ledigt")
-setdata("input_number." + mhostname + "_hdd_ledigt", free_disk, mhostname + " hdd ledigt", "GB", "mdi:harddisk")
+print("input_number." + mhostname + "_hdd_total")
+setdata("input_number." + mhostname + "_hdd_total", total_disk, mhostname + " hdd total", "GB", "mdi:harddisk")
+print("input_number." + mhostname + "_hdd_used")
+setdata("input_number." + mhostname + "_hdd_used", used_disk, mhostname + " hdd used", "GB", "mdi:harddisk")
+print("input_number." + mhostname + "_hdd_free")
+setdata("input_number." + mhostname + "_hdd_free", free_disk, mhostname + " hdd free", "GB", "mdi:harddisk")
